22fce2ebe5d2189fe8741171962f88a58a52a0d4
Nathan220320 <rebours22@gmail.com>
2022-01-30 21:45:33 +0100

    [Modification]
    mise en place du theme sombre pour la div result

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const lune = document.getElementById('lune')
-let resultat = document.getElementsById('result')
+let resultat = document.getElementById('result')
 let click = 0
 
 lune.addEventListener('click', function(){
     click++
     if( click %2 == 1 ){ 
     document.body.style.background = "#141D2F";
-    
+    resultat.style.background = "#1E2A47";
     
     }else{
         document.body.style.background = "#F6F8FF";
+        resultat.style.background = "#FEFEFE";
     }
 })
