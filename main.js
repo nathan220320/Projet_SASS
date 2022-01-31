f6ea53f8f67ae44005f7d37f5576dba9e804edef
Nathan220320 <rebours22@gmail.com>
2022-01-31 10:51:41 +0100

    [Modification]
    Ajout du style de la div search pour le mode sombre

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const lune = document.getElementById('lune')
 let resultat = document.getElementById('result')
+let search = document.querySelector('.search')
+let stats = document.querySelector('.back')
+
 let click = 0
 
 lune.addEventListener('click', function(){
@@ -7,9 +10,13 @@ lune.addEventListener('click', function(){
     if( click %2 == 1 ){ 
     document.body.style.background = "#141D2F";
     resultat.style.background = "#1E2A47";
+    search.style.background = "#1E2A47";
+    stats.style.background = "#141D2F"
     
     }else{
         document.body.style.background = "#F6F8FF";
         resultat.style.background = "#FEFEFE";
+        search.style.background = "#FEFEFE";
+
     }
 })
